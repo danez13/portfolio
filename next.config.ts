dbfc373deb2664d6b24bcba9132960f521d6ecfd
danez13 <hernandezdaniel1702@gmail.com>
2025-07-08 10:10:32 -0400

    feat: update Next.js configuration for GitHub Pages compatibility

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   basePath: "/portfolio",
+  assetPrefix: "/portfolio", // <-- Important
+  images: {
+    unoptimized: true, // <-- Crucial for GitHub Pages
+  },
 };
 
 export default nextConfig;
