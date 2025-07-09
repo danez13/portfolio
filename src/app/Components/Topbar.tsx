835cb5ee25f6fdf4c89a0a6603f14b651c14b7dd
danez13 <hernandezdaniel1702@gmail.com>
2025-07-09 10:02:36 -0400

    refactor: clean up Topbar component by removing commented-out code

--- src/app/Components/Topbar.tsx
+++ src/app/Components/Topbar.tsx
@@ -1,9 +1,6 @@
 import ThemeSwitch from "../theme/ThemeSwitch";
 export default function Topbar(){
     return (
-        // <nav className="pl-5 md:px-15 xl:px-35 py-5 md:py-15 xl:py-13 h-20 lg:h-35 content-center">
-        //     <p className="h-full content-top align-middle text-xl font-black">Daniel Hernandez</p>
-        // </nav>
         <nav className="flex justify-between px-10 md:px-12 h-12 md:h-38 lg:h-14 lg:px-30 xl:px-36 items-end">
             <p className="h-fit font-black text-2xl">Daniel Hernandez</p>
             <ThemeSwitch />
